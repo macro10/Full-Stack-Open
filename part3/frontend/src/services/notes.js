de3ba837438343666fb59e129859f10fb68ce5d6
macro10 <mchaletrotter22@gmail.com>
2025-01-10 20:10:24 -0500

    added part3 containing my deployable notes app

--- part3/frontend/src/services/notes.js
+++ part3/frontend/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://part3-hidden-sun-5253.fly.dev/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
