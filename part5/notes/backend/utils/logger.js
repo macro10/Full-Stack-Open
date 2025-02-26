1759c579d808bed66b80ac644c5f1b01577b9ea1
macro10 <mchaletrotter22@gmail.com>
2025-02-26 14:32:36 -0500

    part5-a changes

---
+++ part5/notes/backend/utils/logger.js
@@ -0,0 +1,19 @@
+/*
+  All printing to the console occurs in this module
+*/
+
+const info = (...params) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
+}
+
+const error = (...params) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
+}
+
+module.exports = {
+  info, error
+}
