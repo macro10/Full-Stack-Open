81c712aa5f4afa3a5903cb5f902bd05aa9c8a5e2
macro10 <mchaletrotter22@gmail.com>
2025-03-02 18:48:23 -0500

    completed ex 5.6 Blog List Frontend, step 6

--- part5/blogList/frontend/src/App.jsx
+++ part5/blogList/frontend/src/App.jsx
@@ -4,12 +4,11 @@ import blogService from './services/blogs'
 import Notification from './components/Notification'
 import loginService from './services/login'
 import Togglable from './components/Togglable'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [newTitle, setNewTitle] = useState('')
-  const [newAuthor, setNewAuthor] = useState('')
-  const [newUrl, setNewUrl] = useState('')
+  
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -56,22 +55,13 @@ const App = () => {
     }
   }
 
-  const addBlog = (event) => {
-    event.preventDefault()
+  const addBlog = (blogObject) => {
     blogFormRef.current.toggleVisibility()
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl,
-    }
 
     blogService
       .create(blogObject)
       .then(returnedBlog => {
         setBlogs(blogs.concat(returnedBlog))
-        setNewTitle('')
-        setNewAuthor('')
-        setNewUrl('')
       })
 
     setErrorMessage(`a new blog ${blogObject.title} by ${blogObject.author} added`)
@@ -183,7 +173,11 @@ const App = () => {
         <button onClick={handleLogout}>
           logout
         </button>
-        {blogForm()}
+        <Togglable buttonLabel="new blog" ref={blogFormRef}>
+        <BlogForm
+          createBlog={addBlog}
+        />
+        </Togglable>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog}/>
         )}
